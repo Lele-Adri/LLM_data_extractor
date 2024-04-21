ef399315cadc01f43916a343b8b0bc5ced16a04c
Alex <xenakis_alexandros@outlook.com>
2024-04-21 23:47:43 +0200

    use urls_list for data extraction

--- app/services/data_extraction_service.py
+++ app/services/data_extraction_service.py
@@ -1,23 +1,23 @@
 import json
-from typing import Dict
+from typing import Dict, List
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.indexes import VectorstoreIndexCreator
+from pydantic import HttpUrl
 
 from models.url_analysis import UrlAnalysisRequestParams
 from helpers.llm_helpers import get_gpt_3
 
 
-async def extract_information_from_url(params: UrlAnalysisRequestParams) -> Dict[str, str]:
-
-    llm = get_gpt_3()
+async def extract_information_from_url(params: UrlAnalysisRequestParams, urls_list: List[HttpUrl]) -> Dict[str, str]:
     
-    # TODO: arg list will be populated with elements of data_to_extract
-    loader = WebBaseLoader(str(params.url))
-    index = VectorstoreIndexCreator().from_loaders([loader])
+    loaders_dict: Dict[HttpUrl, WebBaseLoader] = {}
+    for url in urls_list:
+        loaders_dict[url] = WebBaseLoader(str(url))
+    index = VectorstoreIndexCreator().from_loaders([loader for _, loader in loaders_dict.items()])
 
-    sought_data_str = '\n'.join(f"\t\t'{key}': {value}" for key, value in params.sought_data.items())
+    sought_data_str = '\n'.join(f"\t\t'{key}': {value}" for key, value in params.sought_data.items()) # TODO: maybe use json representation 
     query = f"""
-            From the presented HTML content, extract the following information:
+            From the presented HTML contents, extract the following information:
             {sought_data_str}
             You should output a json of key value pairs, where the key is the name of the data you want to extract and the value is the data you extracted 
             (which is the information described in the values of the previous list entries).
--- app/services/url_analyzer_service.py
+++ app/services/url_analyzer_service.py
@@ -30,3 +30,21 @@ async def scrape_and_extract_data(params: UrlAnalysisRequestParams) -> UrlAnalys
 
 
     return UrlAnalysisResponseModel(extracted_data=extracted_data)
+
+async def scrape_and_extract_data_v1(params: UrlAnalysisRequestParams) -> UrlAnalysisResponseModel:
+    extracted_data: dict[str, str] = {key: "" for key in params.sought_data}
+    links_to_visit: set[HttpUrl] = {params.url}
+    visited_links = set()
+    while len(links_to_visit) > 0:
+        current_link = links_to_visit.pop()
+        visited_links.add(current_link)
+        extracted_links: UrlAnalysisInfoLinks = await download_link_content(current_link)
+        links_of_interest: Tuple[Set[str], Set[HttpUrl]] = await filter_relevant_links_using_title(extracted_links.link_dictionary, extracted_links.titles_set, params.sought_data)
+        # new_links = links_of_interest.link_dictionary.values() - visited_links - links_to_visit
+        # links_to_visit.update(new_links)
+
+    # extract data
+    new_data: Dict[str, str] = await extract_information_from_url(params, []) # TODO: populate the url list
+    extracted_data = update_dict_with_new_data(extracted_data, new_data)
+
+    return UrlAnalysisResponseModel(extracted_data=extracted_data)
