f4339b5a981e7bd186f3921359fd1e405ea9b962
Alex <xenakis_alexandros@outlook.com>
2024-05-18 20:14:03 +0200

    add benchmarking level

--- benchmarks/cProfile_output_helper.py
+++ benchmarks/cProfile_output_helper.py
@@ -10,40 +10,12 @@ non_algo_paths = {
     "venv",
     "benchmarks",
     "test",
-    "Lib"
+    "Lib",
+    "url_analysis_controller"
 }
 algo_paths = {
     "LLM_data_extractor"
 }
-
-def filter_profile_output(profile_output: str) -> str:
-    return "\n".join(line for line in profile_output.split("\n") if should_keep_entry(line))
-
-
-def should_keep_entry(line):
-    dataRow: ProfileOutputDataRow = extract_line_data(line)
-
-    enable_profiling_value = float(os.getenv(ENABLE_PROFILING_ENVIRONMENT_VARIABLE_NAME, 0))
-
-    if dataRow is None:
-        return True
-
-    if enable_profiling_value == 2:
-        try: 
-            if ((dataRow.tottime < time_threshold and 
-                 dataRow.tottime_percall < time_threshold) or
-                dataRow.ncalls == "0"):
-                return False
-        except Exception as e:
-            print(f'caught {type(e)}: e')
-    if enable_profiling_value == 1:
-        path_name = dataRow.filename or dataRow.builtin or ""
-        if (not any(name for name in algo_paths if name in path_name)):
-            return False
-        if any(name for name in non_algo_paths 
-               if name in path_name):
-            return False
-    return True
 
 
 @dataclass
@@ -57,6 +29,49 @@ class ProfileOutputDataRow:
     lineno: int
     function: str
     builtin: str
+
+
+def filter_profile_output(profile_output: str) -> str:
+    return "\n".join(line for line in profile_output.split("\n") if should_keep_entry(line))
+
+
+def should_keep_entry(line):
+    dataRow: ProfileOutputDataRow = extract_line_data(line)
+
+    enable_profiling_value = float(os.getenv(ENABLE_PROFILING_ENVIRONMENT_VARIABLE_NAME, 0))
+
+    if dataRow is None:
+        return True
+    path_name = dataRow.filename or dataRow.builtin or ""
+
+    # 1: keep only algo-paths
+    if enable_profiling_value == 1:
+        if path_includes_excluded_path(path_name) or not path_includes_project_path(path_name):
+            return False
+    # 2: remove non-zero entries
+    if enable_profiling_value == 2:
+        if is_row_below_time_threshold(dataRow):
+            return False
+    # 3: remove non-zero entries & excluded paths
+    if enable_profiling_value == 3:
+        if path_includes_excluded_path(path_name) or is_row_below_time_threshold(dataRow):
+            return False
+    return True
+
+
+def is_row_below_time_threshold(dataRow: ProfileOutputDataRow) -> bool:
+    return ((dataRow.tottime < time_threshold and 
+             dataRow.tottime_percall < time_threshold) or
+            dataRow.ncalls == "0")
+
+def path_includes_project_path(path: str) -> bool: 
+    return path_includes_one_of(path, algo_paths)
+
+def path_includes_excluded_path(path: str) -> bool: 
+    return path_includes_one_of(path, non_algo_paths)
+
+def path_includes_one_of(path, paths_set) -> bool: 
+    return any(_ for _ in paths_set if _ in path)
 
 
 def extract_line_data(line: str) -> tuple:
